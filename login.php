43147fbb4ca102172cb579bfcbdf350b125a89cd
Juan Farias <juanfarias580@gmail.com>
2020-03-30 09:33:36 -0300

    Atualização

--- login.php
+++ login.php
@@ -1,22 +1,22 @@
 <?php
-    session_start();
-    require_once "vendor/autoload.php";
-    
-    $logar = new App\Model\Logar();
-    $logarDao = new App\Model\LogarDaoMysql();
-    
-    if($_SERVER['REQUEST_METHOD'] == "POST") {
-        $logar->setEmail($_REQUEST['email']);
-        $logar->setSenha($_REQUEST['senha']);
-        
-        if($logarDao->login($logar)) {
-            die(header("Location: http://".$_SERVER['HTTP_HOST']."/crud"));
-        } else {
-            die(header("Location: http://".$_SERVER['HTTP_HOST']."/crud/login"));
-        }
+session_start();
+require_once "vendor/autoload.php";
+
+$logar = new App\Model\Logar();
+$logarDao = new App\Model\LogarDaoMysql();
+
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+    $logar->setEmail($_REQUEST['email']);
+    $logar->setSenha($_REQUEST['senha']);
+
+    if($logarDao->login($logar)) {
+        die(header("Location: http://".$_SERVER['HTTP_HOST']."/crud"));
+    } else {
+        die(header("Location: http://".$_SERVER['HTTP_HOST']."/crud/login"));
     }
-    
-    $logarDao->logout();
+}
+
+$logarDao->logout();
 ?>
 
 <?php include_once "inc/header.php"; ?>
