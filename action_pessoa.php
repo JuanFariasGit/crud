f2c385a471c73567b89f7748a8c66e2f93c67939
Juan Farias <juanfarias580@gmail.com>
2020-04-12 18:54:42 -0300

    Atualização

--- action_pessoa.php
+++ action_pessoa.php
@@ -1,38 +1,39 @@
 <?php
-include_once "inc/check_login.php";
+declare(strict_types=1);
 
-if($logarDao->checkLogin()) {
-    $pessoa    = new App\Model\Pessoa();
-    $pessoaDao = new App\Model\PessoaDaoMysql();
-    
-    switch($_GET['option']) {
-        case "create":
-            if($_SERVER["REQUEST_METHOD"] == "POST") {
-                $pessoa->setCpf($_REQUEST['cpf']);
-                $pessoa->setNome($_REQUEST['nome']);
-                $pessoaDao->create($pessoa);
-                die(header("Location: http://".$_SERVER['HTTP_HOST']."/crud"));
-            } 
-        break;    
+require_once "vendor/autoload.php";
 
-        case "update":
-            if($_SERVER["REQUEST_METHOD"] == "POST") {
-                $pessoa->setId($_REQUEST['id']);
-                $pessoa->setCpf($_REQUEST['cpf']);
-                $pessoa->setNome($_REQUEST['nome']);
-                $pessoaDao->update($pessoa);
-                die(header("Location: http://".$_SERVER['HTTP_HOST']."/crud"));
-            } 
-        break;
+$pessoa    = new App\Model\Pessoa();
+$pessoaDao = new App\Model\PessoaDaoMysql();
 
-       case "delete":
-            if($_SERVER['REQUEST_METHOD'] == "GET") {
-                $pessoaDao->delete($_REQUEST['id']);        
-                die(header("Location: http://".$_SERVER['HTTP_HOST']."/crud"));
-            }
-        break;
-    }
+switch($_GET['option']) {
+    case "create":
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            $pessoa->setCpf($_REQUEST['cpf']);
+            $pessoa->setNome($_REQUEST['nome']);
 
-} else {
-    die(header("Location: http://".$_SERVER['HTTP_HOST']."/crud/login"));
+            $pessoaDao->create($pessoa);
+            die(header("Location: http://".$_SERVER['HTTP_HOST']."/crud"));
+        } 
+    break;    
+
+    case "update":
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            $pessoa->setId($_REQUEST['id']);
+            $pessoa->setCpf($_REQUEST['cpf']);
+            $pessoa->setNome($_REQUEST['nome']);
+            
+            $pessoaDao->update($pessoa);
+            die(header("Location: http://".$_SERVER['HTTP_HOST']."/crud"));
+        } 
+    break;
+
+    case "delete":
+        if($_SERVER['REQUEST_METHOD'] == "GET") {
+            $pessoaDao->delete($_REQUEST['id']);        
+            die(header("Location: http://".$_SERVER['HTTP_HOST']."/crud"));
+        }
+    break;
 }
+
+
