24ae0b8a03176309ef3c5692154e02f0f460d77b
Juan Farias <juanfarias580@gmail.com>
2020-04-27 10:55:21 -0300

    Atualização

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,3 @@
-<script src="http://<?= $_SERVER['HTTP_HOST']; ?>/crud/assets/js/jquery.min.js"></script>
+<script src="http://<?= $_SERVER['HTTP_HOST'].'/'.explode('/', $_SERVER['REQUEST_URI'])[1]; ?>/assets/js/jquery.min.js"></script>
 </body>
 </html>
--- inc/header.php
+++ inc/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST']; ?>/crud/assets/css/style.css">
+    <link rel="stylesheet" href="http://<?= $_SERVER['HTTP_HOST'].'/'.explode('/', $_SERVER['REQUEST_URI'])[1]; ?>/assets/css/style.css">
     <title>PHP Da Maneira Certa - CRUD</title>
 </head>
 <body>
